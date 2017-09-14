5d7b5dce60d28ba755bd5324929aaf4043b61300
Vic <vic@vic.local>
2017-09-14 13:38:16 +0800

    Initial Commit

---
+++ C 阶乘/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C 阶乘
+//
+//  Created by Vic on 2017/9/14.
+//  Copyright © 2017年 Vic. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
